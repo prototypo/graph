5953a63e27a9dae98f14422ba234d369bc9d28d6
Ryan Caught <rcaught@gmail.com>
2016-04-15 15:39:59 +1000

    Quick and dirty class level storage

--- ruby/server.rb
+++ ruby/server.rb
@@ -1,19 +1,33 @@
 require 'sinatra'
 require 'json'
 require './lib/map'
+require 'awesome_print'
+
+before do
+  if !defined?(@@map)
+    @@map = Map.new
+    @@map.load_nodes([])
+    @@map.process_nodes
+  end
+end
 
 post '/' do
   data = JSON.parse(request.body.read)
 
-  @map = Map.new
-  @map.load_nodes(Map.example_nodes)
-  @map.process_nodes
+  if data['map']
+    @@map.load_nodes(data['map'])
+    @@map.process_nodes
+  end
 
   if data['start'] && data['end']
-    if @map.nodes.keys.include?(data['start']) && @map.nodes.keys.include?(data['end'])
-      return { 'distance' => @map.shortest_distance(data['start'], data['end']) }.to_json
+    if @@map.nodes.keys.include?(data['start']) && @@map.nodes.keys.include?(data['end'])
+      return { 'distance' => @@map.shortest_distance(data['start'], data['end']) }.to_json
     else
-      # 40X
+      status 400
     end
   end
+end
+
+get '/' do
+  @@map.nodes.ai(html: true)
 end
